dcaf0a3db1acf4ff97234da1d687361b204fcc15
Theia137 <157898675+Theia137@users.noreply.github.com>
2026-07-09 18:36:46 +0900

    Add: shortest-path-in-binary-matrix - submission-5

---
+++ Data Structures & Algorithms/shortest-path-in-binary-matrix/submission-5.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    queue<tuple<int, int, int>> q;
+    vector<int> dy = {-1, -1, -1, 0, 0, 1, 1, 1};
+    vector<int> dx = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+    int m, n;
+
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+
+        if(grid[0][0] || grid[m-1][n-1]){
+            return -1;
+        }
+
+        int ans = 1;
+        grid[0][0] = 1;
+        q.push({1, 0, 0});
+
+        while(!q.empty()){
+            int depth = get<0>(q.front());
+            int y = get<1>(q.front());
+            int x = get<2>(q.front());
+            if(y == m-1 && x == n-1){
+                return depth;
+            }
+            q.pop();
+
+            for(int i=0; i<8; i++){
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if(0<=ny && ny<m && 0<=nx && nx<n && grid[ny][nx] == 0){
+                    grid[ny][nx] = 1;
+                    q.push({depth + 1, ny, nx});
+                }
+            }
+        }
+        return -1;
+    }
+};
