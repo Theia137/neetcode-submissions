49d847ce98b09501a435dc75c426d0763604935e
Theia137 <157898675+Theia137@users.noreply.github.com>
2026-07-10 09:38:30 +0900

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> mapp;
+    Node* cloneGraph(Node* node) {
+        if(node == nullptr){
+            return nullptr;
+        }
+        else{
+            if(mapp[node] == nullptr){
+                Node* newnode = new Node(node->val);
+                mapp[node] = newnode;
+                for(Node* neighbor : node->neighbors){
+                    newnode->neighbors.push_back(cloneGraph(neighbor));
+                }
+                return newnode;
+            }
+            else{
+                return mapp[node];
+            }
+        }
+    }
+};
